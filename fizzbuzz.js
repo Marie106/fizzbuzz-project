9f116e2b7c382f9ec533c3a5010d4072b66cbb88
Marie106 <Mariebangoura404@gmail.com>
2024-07-03 01:29:53 -0400

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -13,9 +13,26 @@ for (let i = 1; i <=100; i++) {
     }  else if (1 % 3 === 0) {
         console.log("Fizz");
     }   else if (i % 5 === 0) {
-        console.log("Buzz");
+       console.log("Buzz");
     }
     else {
         console.log(i)
-    }
+  }
+}
+
+
+let i = 1;
+while  (i<=100) {
+if (i % 3 === 0 && i % 5 === 0) {
+
+    console.log("Fizz Buzz");
+}  else if (1 % 3 === 0) {
+    console.log("Fizz");
+}   else if (i % 5 === 0) {
+    console.log("Buzz");
+}
+else {
+    console.log(i)
+}
+i++;
 }
